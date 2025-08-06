ab7f1f7f4ae816218fc1cdacba87081f244aa947
Magooch <crvallecito@gmail.com>
2025-08-06 11:51:52 -0600

    Complete number filter problem

--- initial_coding_problems/number_filter.c
+++ initial_coding_problems/number_filter.c
@@ -2,6 +2,34 @@
 #include <stdlib.h>
 
 int main() {
-    printf("Hello, world!\n");
-    return 0;
+    int input_array[5]; // Stores user input
+
+    for (int i = 0; i < 5; i++) {
+        printf("Enter a number: ");
+        scanf("%d", &input_array[i]);
+    }
+
+    int iterator = 0;
+
+    int array_length = sizeof(input_array) / sizeof(input_array[0]);
+    for (int i = 0; i < array_length; i++) {
+        if (input_array[i] > 50) {
+            iterator += 1;
+        }
+    }
+    
+    int over_50_iterator = 0;
+    int over_50_array[iterator];
+    for (int i = 0; i < array_length; i++) {
+        if (input_array[i] > 50) {
+            over_50_array[over_50_iterator] = input_array[i];
+            over_50_iterator += 1;
+        }
+    }
+
+    puts("The numbers you chose that are over 50 are: ");
+    for (int i = 0; i < iterator; i++) {
+        printf("%d\n", over_50_array[i]);
+    }
+
 }
