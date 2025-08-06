2ef1a0f9c4ea0e59d99c98ccb6985355c6362dd7
Magooch <crvallecito@gmail.com>
2025-08-06 13:41:06 -0600

    Print out formatted strings, in correct order

--- initial_coding_problems/grid_printer.c
+++ initial_coding_problems/grid_printer.c
@@ -3,6 +3,22 @@
 #include <string.h>
 
 int main() {
-    printf("Hello, world!\n");
-    return 0;
+    char *matrix[3][4];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            char input[10];
+            sprintf(input, "Row%dCol%d", i, j);
+            matrix[i][j] = input;
+        }
+    }
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (j == 3) {
+                printf("%s\n", matrix[i][j]);
+            } else {
+                printf("%s, ", matrix[i][j]);
+            }
+        }
+    }
 }
