1bc2e3d75ccd9aee865114077ce0337ebc9d195d
Magooch <crvallecito@gmail.com>
2025-08-06 00:00:05 -0600

    Add comments

--- initial_coding_problems/reverse_string.c
+++ initial_coding_problems/reverse_string.c
@@ -3,14 +3,19 @@
 #include <string.h>
 
 int main() {
-    char reversed_string[10];
+    // Requests user input and store in 'username'
     char username[10];
     printf("Enter your name: ");
     scanf("%s", username);
-    int index = strlen(username) - 1;
+
+    char reversed_string[10]; // initialize empty string for future reversed string
+    int index = strlen(username) - 1; // Set index as length of username - 1
+
+    // Populate reversed_string with reversed version of username
     for (int i = 0; i < strlen(username); i++) {
         reversed_string[i] = username[index - i];
     }
+
     printf("Your name is: %s\n", reversed_string);
     return 0;
 }
