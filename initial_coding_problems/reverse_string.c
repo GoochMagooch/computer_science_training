67d0177d1ce4b581a9e907f0eb2b6f6de85b2556
Magooch <crvallecito@gmail.com>
2025-08-05 18:15:55 -0600

    Create reverse string file

---
+++ initial_coding_problems/reverse_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    char username[10];
+    printf("Enter your name: ");
+    scanf("%s", username);
+    printf("Your name is %s!\n", username);
+    
+    return 0;
+}
