ff732e5c7a83572955113c8874c01a6387090a5b
Magooch <crvallecito@gmail.com>
2025-08-05 18:43:52 -0600

    Traverse through username

--- initial_coding_problems/reverse_string.c
+++ initial_coding_problems/reverse_string.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
 
+    // char reversed_string[] = {'/0'};
     char username[10];
     printf("Enter your name: ");
     scanf("%s", username);
-    printf("Your name is %s!\n", username);
+    for (int i = 0; i < strlen(username); i++) {
+        printf("%c\n", username[i]);
+    }
+    // int length = sizeof(username) / sizeof(username[0]);
+    // printf(length);
+
     
     return 0;
 }
