088d81e1da2049ba8f0f9be4b779c4a9f37a6297
Magooch <crvallecito@gmail.com>
2025-08-05 20:46:03 -0600

    Solve reverse string problem

--- initial_coding_problems/reverse_string.c
+++ initial_coding_problems/reverse_string.c
@@ -4,16 +4,17 @@
 
 int main() {
 
-    // char reversed_string[] = {'/0'};
+    char reversed_string[10];
     char username[10];
     printf("Enter your name: ");
     scanf("%s", username);
+    int index = strlen(username) - 1;
     for (int i = 0; i < strlen(username); i++) {
-        printf("%c\n", username[i]);
+        reversed_string[i] = username[index - i];
     }
     // int length = sizeof(username) / sizeof(username[0]);
     // printf(length);
-
-    
+    printf("Your name is: %s\n", reversed_string);
     return 0;
+
 }
