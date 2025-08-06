34304ed483dc56e3b3a0a2ed40476a612b790d02
Magooch <crvallecito@gmail.com>
2025-08-06 16:56:41 -0600

    Populate empty 2D array, with logical error in outputting 2D array cells

--- initial_coding_problems/grid_printer.c
+++ initial_coding_problems/grid_printer.c
@@ -3,22 +3,23 @@
 #include <string.h>
 
 int main() {
+
     char *matrix[3][4];
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 4; j++) {
-            char input[10];
-            sprintf(input, "Row%dCol%d", i, j);
+            char input[10] = "";
+            sprintf(input, "Row%dCol%d", (i+1), (j+1));
+            // matrix[i][j] = input;
             matrix[i][j] = input;
+            // printf("%s\n", matrix[i][j]);
         }
     }
-    
+
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 4; j++) {
-            if (j == 3) {
-                printf("%s\n", matrix[i][j]);
-            } else {
-                printf("%s, ", matrix[i][j]);
-            }
+            printf("%s ", matrix[i][j]);
         }
+        printf("\n");
     }
+
 }
