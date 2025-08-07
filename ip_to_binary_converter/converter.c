c5d4f051724da58a9f8a2f5b9b6eb6a5d8422665
Magooch <crvallecito@gmail.com>
2025-08-06 22:23:55 -0600

    Request user inputs and output as formatted IP address

--- ip_to_binary_converter/converter.c
+++ ip_to_binary_converter/converter.c
@@ -3,6 +3,19 @@
 #include <string.h>
 
 int main() {
-    printf("Hello, world!");
+    int ip[4];
+    for (int i = 0; i < 4; i++) {
+        printf("Enter octet %d: ", (i+1));
+        scanf("%d", &ip[i]);
+    }
+
+    int length = sizeof(ip) / sizeof(ip[0]);
+    for (int i = 0; i < length; i++) {
+        if (i == length - 1) {
+            printf("%d\n", ip[i]);
+        } else {
+            printf("%d.", ip[i]);
+        }
+    }
     return 0;
 }
