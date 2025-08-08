24bd80f64a5f3b883fa10b3f1a9760088a59e8b1
Magooch <crvallecito@gmail.com>
2025-08-08 00:07:49 -0600

    Add check for octets higher than 255

--- ip_to_binary_converter/converter.c
+++ ip_to_binary_converter/converter.c
@@ -3,12 +3,23 @@
 #include <string.h>
 
 int main() {
+    int bits[8] = {1, 2, 4, 8, 16, 32, 64, 132}; // Array of bits to run against octets
+
+    // Requests user input and stores inputs in array of ints
     int ip[4];
     for (int i = 0; i < 4; i++) {
+        int octet;
         printf("Enter octet %d: ", (i+1));
-        scanf("%d", &ip[i]);
+        scanf("%d", &octet);
+        if (octet > 255) {
+            printf("Error: octet must be between 0 and 255\n");
+            return 0;
+        } else {
+            ip[i] = octet;
+        }
     }
 
+    // Outputs user inputs formatted as 8 bit IP address
     int length = sizeof(ip) / sizeof(ip[0]);
     for (int i = 0; i < length; i++) {
         if (i == length - 1) {
@@ -17,5 +28,9 @@ int main() {
             printf("%d.", ip[i]);
         }
     }
+
+    // Converts user IP address to binary
+
+
     return 0;
 }
